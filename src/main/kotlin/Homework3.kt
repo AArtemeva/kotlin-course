851624f23ae034cbcc519f12e5205fbfd955f575
Anastasiia Artemyeva <anastasiia.artemyeva@gmail.com>
2024-09-15 16:06:55 +0300

    finish homework

---
+++ src/main/kotlin/Homework3.kt
@@ -0,0 +1,24 @@
+package org.artemyeva
+
+class Homework3 {
+    val event: String = "Hakaton" //  Название мероприятия
+    var date: String = "10.10.2024" // Дата проведения
+    val place: String = "Moscow" // Место проведения
+    private val budget: Int = 1000000 // Бюджет
+    var member: Int = 50 // Количество участников (может измениться)
+    var duration: Long = 5 * 60 // Длительность мероприятия(может измениться)
+    var contactInformation: String = "Moscow 123" //Контактная информация
+    var status: String = "On" //Статус мероприятия
+    val sponsors: String = "sponsor1" // Спонсоры мероприятия
+    var internetAccessLevel: String = "high" //Уровень доступа к интернету
+    var transport: String = "on way" // Информация о транспортировке
+    var numberOfMember: Int = 0 // Количество команд
+    val task: String = "all" // Перечень задач
+    val evacuationPlan: String = "run" //План эвакуации
+    var listOfAvailableEquipment: String = "equipment1" //Список доступного оборудования
+    var listOfFreeEquipment: String = "equipment2"//Список свободного оборудования
+    val planInCaseOfFailure: String = "cry"//План на случай сбоя мероприятия
+    val specialGuest: String = "Leps" //Специальный гость
+    lateinit var mood: String //Настроение по результатам опроса
+    var plan: String = "happy"//Подробный план мероприятия
+}
---
+++ src/main/kotlin/Homework4.kt
@@ -0,0 +1,45 @@
+package org.artemyeva
+
+class Homework4 {
+    val int: Int = 42
+    val long: Long = 98765432123456789L
+    val float: Float = 23.45f
+    val double: Double = 0.123456789
+    val text: String = "Kotlin & Java"
+    val boolean: Boolean = false
+    val letter: Char = 'c'
+    val int1: Int = 500
+    val long1: Long = 4294967296L
+    val float1: Float = 18.0f
+    val double1: Double= -0.001
+    val text1: String = "OpenAI"
+    val text2: String = "true"
+    val letter1: Char = '9'
+    val short: Short = 2048
+    val long2: Long = 10000000000L
+    val float2: Float = 5.75f
+    val double2: Double=1.414
+    val text3: String = "Artificial intelligence"
+    val boolean1: Boolean = false
+    val symbols: Char = '@'
+    val short1: Short = 1024
+    val long3: Long = 1234567890123L
+    val float3: Float = 10.01f
+    val double3: Double = -273.15
+    val text4: String = "SpaceX"
+    val boolean2: Boolean = true
+    val text5: String = "смайл" // двойные кавычки ""
+    val letter2:  = '65535' //кавычки '' Для Char слишком много символов, для String ошибка в кавычках
+    val long4: Long = 72057594037927935L
+    val float4: Float = 2.71828f
+    val double4: Double = 101.0101
+    val text6: String = "Quantum Computing"
+    val boolean3: Boolean = false
+    val letter3: Char = 'x'
+    val int3: Int = 14
+    val long5: Long = 123456789123456789L
+    val float5: Float = 6.626f
+    val double5: Double = 0.007
+    val text7: String = "Android Studio"
+
+}
