71422dcd1730a9c97f9b7a6660bc7f95dcb6246d
Anastasiia Artemyeva <anastasiia.artemyeva@gmail.com>
2024-09-29 15:00:30 +0300

    Homework

---
+++ src/main/kotlin/Lesson8.kt
@@ -0,0 +1,78 @@
+package org.artemyeva
+
+class Lesson8 {
+    fun analyze(originalString: String): String {
+        return if (originalString.contains("невозможно", true)) {
+            originalString.replace(oldValue = "невозможно", newValue = "совершенно точно возможно, просто требует времени")
+        } else if (originalString.startsWith("Я не уверен")) {
+            "$originalString, но моя интуиция говорит об обратном"
+        } else if (originalString.contains("катастрофа", true)) {
+            originalString.replace(oldValue = "катастрофа", "интересное событие")
+        } else if (originalString.endsWith("без проблем")) {
+            originalString.replace(oldValue = "без проблем", "с парой интересных вызовов на пути")
+        } else if (!originalString.contains(" ")) {
+            "Иногда,$originalString, но не всегда"
+        } else {
+            ""
+        }
+    }
+
+    //Задача 1
+    fun dateFromString () {
+        val stringLog = "Пользователь вошел в систему -> 2021-12-01 09:48:23"
+        val splitted = stringLog.split("-> ")[1]
+        println(splitted)
+        val splitted2 = splitted.split(" ")
+        println(splitted2[0])
+        println(splitted2[1])
+    }
+    //Задача 2
+    fun cardMasking () {
+        val cardNumber = "4539 1488 0343 6467"
+        val last4digits = cardNumber.split(" ")[3]
+        println(last4digits)
+        val cardMasked = "**** **** **** $last4digits"
+        println(cardMasked)
+    }
+    //Задача 3
+    fun mail () {
+        val email = "username@example.com"
+        val result = email.replace(oldValue = "@", newValue = " [at] ")
+        val result2 = result.replace(oldValue = ".", newValue = " [dot] ")
+        println(result)
+        println(result2)
+    }
+    //Задача 4
+    fun extractFileName() {
+        val way = "C:/Пользователи/Документы/report.txt"
+        val fileName = way.split("/")[3]
+        println(fileName)
+    }
+    //Задача 5
+    fun phrase() {
+        val abbreviation = "Объектно-ориентированное программирование"
+        val abr1 = abbreviation.split("-", " ")
+        println(abr1)
+        var result: String
+        for (s in abr1) {
+            println(s.first().uppercase())
+            val result1 = ""
+            val result2 = ""
+            val result3 = ""
+        }
+    }
+}
+
+fun main() {
+    val lesson8 = Lesson8()
+//    println(lesson8.analyze("Это невозможно выполнить за один день"))
+//    println(lesson8.analyze("Я не уверен в успехе этого проекта"))
+//    println(lesson8.analyze("Произошла катастрофа на сервере"))
+//    println(lesson8.analyze("Этот код работает без проблем"))
+//    println(lesson8.analyze("Удача"))
+//    lesson8.dateFromString()
+//    lesson8.cardMasking()
+//    lesson8.mail()
+//    lesson8.extractFileName()
+    lesson8.phrase()
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ package org.artemyeva
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+fun task1() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
