62eb8e0d04b1954e40d75d633e04ad53e6c55ccf
Anastasiia Artemyeva <anastasiia.artemyeva@gmail.com>
2024-10-06 17:28:06 +0300

    commit

--- src/main/kotlin/Lesson10_Lists.kt
+++ src/main/kotlin/Lesson10_Lists.kt
@@ -1,66 +1,113 @@
 package org.artemyeva
 
+class Lesson10_Lists {
+
+    //Задача 1. Создание пустого списка
+    fun listTask1() {
+        val listTask1 = listOf<Int>()
+        println("listTask1 $listTask1")
+    }
+
+    //Задача 2. Создание списка со значениями
+    fun listTask2() {
+        val listTask2 = listOf("Hello", "World", "Kotlin")
+        println("listTask2 $listTask2")
+    }
+
+    //Задача 3. Создание изменяемого списка
+    fun listTask3() {
+        val listTask3 = mutableListOf(1, 2, 3, 4, 5)
+        println("listTask3 $listTask3")
+    }
+
+    //Задача 4. Добавление элементов в список
+    fun listTask4() {
+        val listTask4 = mutableListOf(1, 2, 3, 4, 5)
+        listTask4.add(6)
+        listTask4.add(7)
+        listTask4.add(8)
+        println("listTask4 $listTask4")
+    }
+
+    //Задача 5. Удаление элемента из списка
+    fun listTask5() {
+        val listTask5 = mutableListOf("Hello", "World", "Kotlin")
+        listTask5.remove("World")
+        println("listTask5 $listTask5")
+    }
+
+    //Задача 6. Перебор элементов списка
+    fun listTask6() {
+        val listTask6 = mutableListOf(1, 2, 3, 4, 5)
+        println("listTask6 $listTask6")
+        for (elem in listTask6) {
+            println(elem)
+        }
+    }
+
+    //Задача 7. Доступ к элементу по индексу
+    fun listTask7() {
+        val listTask7 = mutableListOf("Hello", "World", "Kotlin")
+        println("listTask7 $listTask7")
+        println("elem 2 ${listTask7[1]}")
+    }
+
+    //Задача 8. Изменение элемента по индексу
+    fun listTask8() {
+        val listTask8 = mutableListOf(1, 2, 3, 4, 5)
+        println("listTask8 $listTask8")
+        listTask8[2] = 100
+        println("new listTask8 $listTask8")
+    }
+
+    //Задача 9. Объединение двух списков
+    fun listTask9() {
+        val listTask9First = mutableListOf("Hello", "World", "Kotlin")
+        val listTask9Second = mutableListOf("dfgdf", "dgf", "dgf")
+        val listTask9Result = mutableListOf<String>()
+        for (elem in listTask9First) {
+            listTask9Result.add(elem)
+        }
+        for (elem in listTask9Second) {
+            listTask9Result.add(elem)
+        }
+        println("listTask9First $listTask9First listTask9Second $listTask9Second listTask9Result $listTask9Result")
+    }
+
+    //Задача 10. Поиск минимального и максимального элемента
+    fun listTask10() {
+        val listTask10 = mutableListOf(2, 1, 2, 3, 4, 5, 3)
+        var min = listTask10[0]
+        var max = listTask10[0]
+        for (elem in listTask10) {
+            if (min > elem) min = elem
+            if (max < elem) max = elem
+        }
+        println("listTask10 $listTask10  -- min $min  max $max")
+    }
+
+    //Задача 11. Фильтрация списка
+    fun listTask11() {
+        val listTask11 = mutableListOf(2, 1, 2, 3, 4, 5, 3)
+        val listTask11result = mutableListOf<Int>()
+        for (elem in listTask11) {
+            if (elem % 2 == 0) listTask11result.add(elem)
+        }
+        println("listTask11 $listTask11 -> listTask11result $listTask11result")
+    }
+}
+
 fun main() {
-
-    val listTask1 = listOf<Int>()
-    println("listTask1 $listTask1")
-
-    val listTask2 = listOf("Hello", "World", "Kotlin")
-    println("listTask2 $listTask2")
-
-    val listTask3 = mutableListOf(1, 2, 3, 4, 5)
-    println("listTask3 $listTask3")
-
-    val listTask4 = mutableListOf(1, 2, 3, 4, 5)
-    listTask4.add(6)
-    listTask4.add(7)
-    listTask4.add(8)
-    println("listTask4 $listTask4")
-
-    val listTask5 = mutableListOf("Hello", "World", "Kotlin")
-    listTask5.remove("World")
-    println("listTask5 $listTask5")
-
-    val listTask6 = mutableListOf(1, 2, 3, 4, 5)
-    println("listTask6 $listTask6")
-    for (elem in listTask6) {
-        println(elem)
-    }
-
-    val listTask7 = mutableListOf("Hello", "World", "Kotlin")
-    println("listTask7 $listTask7")
-    println("elem 2 ${listTask7[1]}")
-
-    val listTask8 = mutableListOf(1, 2, 3, 4, 5)
-    println("listTask8 $listTask8")
-    listTask8[2] = 100
-    println("new listTask8 $listTask8")
-
-    val listTask9First = mutableListOf("Hello", "World", "Kotlin")
-    val listTask9Second = mutableListOf("dfgdf", "dgf", "dgf")
-    val listTask9Result = mutableListOf<String>()
-    for (elem in listTask9First) {
-        listTask9Result.add(elem)
-    }
-    for (elem in listTask9Second) {
-        listTask9Result.add(elem)
-    }
-    println("listTask9First $listTask9First listTask9Second $listTask9Second listTask9Result $listTask9Result")
-
-    val listTask10 = mutableListOf(2, 1, 2, 3, 4, 5, 3)
-    var min = listTask10[0]
-    var max = listTask10[0]
-    for (elem in listTask10) {
-        if (min > elem) min = elem
-        if (max < elem) max = elem
-    }
-    println("listTask10 $listTask10  -- min $min  max $max")
-
-    val listTask11 = mutableListOf(2, 1, 2, 3, 4, 5, 3)
-    val listTask11result = mutableListOf<Int>()
-    for (elem in listTask11) {
-        if (elem % 2 == 0) listTask11result.add(elem)
-    }
-    println("listTask11 $listTask11 -> listTask11result $listTask11result")
-
+    val lesson11 = Lesson10_Lists()
+    lesson11.listTask1()
+    lesson11.listTask2()
+    lesson11.listTask3()
+    lesson11.listTask4()
+    lesson11.listTask5()
+    lesson11.listTask6()
+    lesson11.listTask7()
+    lesson11.listTask8()
+    lesson11.listTask9()
+    lesson11.listTask10()
+    lesson11.listTask11()
 }
--- src/main/kotlin/Lesson10_Sets.kt
+++ src/main/kotlin/Lesson10_Sets.kt
@@ -1,93 +1,132 @@
 package org.artemyeva
 
-fun main() {
-    val setTask1 = setOf<Int>()
-    println("setTask1 $setTask1")
-    println()
+class Lesson10_Sets {
 
-    val setTask2 = setOf(1, 2, 3)
-    println("setTask2 $setTask2")
-    println()
-
-    val setTask3 = mutableSetOf("Kotlin", "Java", "Scala")
-    println("setTask3 $setTask3")
-    println()
-
-    val setTask4 = mutableSetOf("Kotlin", "Java", "Scala")
-    setTask4.add("Swift")
-    setTask4.add("Go")
-    println("setTask4 $setTask4")
-    println()
-
-    val setTask5 = mutableSetOf(1, 2, 3)
-    setTask5.remove(2)
-    println("setTask5 $setTask5")
-    println()
-
-    val setTask6 = mutableSetOf(1, 2, 3)
-    for (elem in setTask6) {
-        println(elem)
+    //Задача 1. Создание пустого множества
+    fun setTask1() {
+        val setTask1 = setOf<Int>()
+        println("setTask1 $setTask1")
     }
-    println()
 
-    val setTask7 = mutableSetOf("Kotlin", "Java", "Scala")
-    println("setTask7 $setTask7 -> Scala -> ${checkSet(setTask7, "Scala")}")
-    println("setTask7 $setTask7 -> dsfsd -> ${checkSet(setTask7, "dsfsd")}")
-    println()
-
-    val setTask8First = mutableSetOf("Kotlin", "Java", "Scala")
-    val setTask8Second = mutableSetOf("fghf", "Java", "Syut")
-    val setTask8Result = mutableSetOf<String>()
-    for (elem in setTask8First) {
-        setTask8Result.add(elem)
+    //Задача 2. Создание множества со значениями
+    fun setTask2() {
+        val setTask2 = setOf(1, 2, 3)
+        println("setTask2 $setTask2")
     }
-    for (elem in setTask8Second) {
-        setTask8Result.add(elem)
-    }
-    println("setTask8First $setTask8First setTask8Second $setTask8Second setTask8Result $setTask8Result")
-    println()
 
-    val setTask9First = mutableSetOf(1, 2, 3, 4)
-    val setTask9Second = mutableSetOf(3, 4, 5, 6)
-    val setTask9Result = mutableSetOf<Int>()
-    for (elem1 in setTask9First) {
-        for (elem2 in setTask9Second) {
-            if (elem1 == elem2) {
-                setTask9Result.add(elem2)
-            }
+    //Задача 3. Создание изменяемого множества
+    fun setTask3() {
+        val setTask3 = mutableSetOf("Kotlin", "Java", "Scala")
+        println("setTask3 $setTask3")
+    }
+
+    //Задача 4. Добавление элементов в множество
+    fun setTask4() {
+        val setTask4 = mutableSetOf("Kotlin", "Java", "Scala")
+        setTask4.add("Swift")
+        setTask4.add("Go")
+        println("setTask4 $setTask4")
+    }
+
+    //Задача 5. Удаление элемента из множества
+    fun setTask5() {
+        val setTask5 = mutableSetOf(1, 2, 3)
+        setTask5.remove(2)
+        println("setTask5 $setTask5")
+    }
+
+    //Задача 6. Перебор элементов множества
+    fun setTask6() {
+        val setTask6 = mutableSetOf(1, 2, 3)
+        for (elem in setTask6) {
+            println(elem)
         }
     }
-    println("setTask9First $setTask9First setTask9Second $setTask9Second setTask9Result $setTask9Result")
-    println()
 
-    val setTask10First = mutableSetOf("Kotlin", "Java", "Scala")
-    val setTask10Second = mutableSetOf("fghf", "Java", "Syut")
-    val setTask10Result = mutableSetOf<String>()
-    for (elem in setTask10First) {
-        var flag = false
-        for (elem2 in setTask10Second) {
-            if (elem == elem2) {
-                flag = true
-                break
+    //Задача 7. Проверка наличия элемента в множестве
+    fun setTask7() {
+        val setTask7 = mutableSetOf("Kotlin", "Java", "Scala")
+        println("setTask7 $setTask7 -> Scala -> ${checkSet(setTask7, "Scala")}")
+        println("setTask7 $setTask7 -> dsfsd -> ${checkSet(setTask7, "dsfsd")}")
+    }
+
+    //Задача 8. Объединение двух множеств
+    fun setTask8() {
+        val setTask8First = mutableSetOf("Kotlin", "Java", "Scala")
+        val setTask8Second = mutableSetOf("fghf", "Java", "Syut")
+        val setTask8Result = mutableSetOf<String>()
+        for (elem in setTask8First) {
+            setTask8Result.add(elem)
+        }
+        for (elem in setTask8Second) {
+            setTask8Result.add(elem)
+        }
+        println("setTask8First $setTask8First setTask8Second $setTask8Second setTask8Result $setTask8Result")
+    }
+
+    //Задача 9. Пересечение двух множеств
+    fun setTask9() {
+        val setTask9First = mutableSetOf(1, 2, 3, 4)
+        val setTask9Second = mutableSetOf(3, 4, 5, 6)
+        val setTask9Result = mutableSetOf<Int>()
+        for (elem1 in setTask9First) {
+            for (elem2 in setTask9Second) {
+                if (elem1 == elem2) {
+                    setTask9Result.add(elem2)
+                }
             }
         }
-        if (!flag) setTask10Result.add(elem)
+        println("setTask9First $setTask9First setTask9Second $setTask9Second setTask9Result $setTask9Result")
     }
-    println("setTask10First $setTask10First setTask10Second $setTask10Second setTask10Result $setTask10Result")
-    println()
 
-    val setTask11 = mutableSetOf("Kotlin", "Java", "Scala")
-    val setTask11List = mutableListOf<String>()
-    for (elem in setTask11) {
-        setTask11List.add(elem)
+    //Задача 10. Разность двух множеств
+    fun setTask10() {
+        val setTask10First = mutableSetOf("Kotlin", "Java", "Scala")
+        val setTask10Second = mutableSetOf("fghf", "Java", "Syut")
+        val setTask10Result = mutableSetOf<String>()
+        for (elem in setTask10First) {
+            var flag = false
+            for (elem2 in setTask10Second) {
+                if (elem == elem2) {
+                    flag = true
+                    break
+                }
+            }
+            if (!flag) setTask10Result.add(elem)
+        }
+        println("setTask10First $setTask10First setTask10Second $setTask10Second setTask10Result $setTask10Result")
     }
-    println("setTask11 $setTask11 setTask11List $setTask11List")
 
+    //Задача 11. Преобразование множества в список
+    fun setTask11() {
+        val setTask11 = mutableSetOf("Kotlin", "Java", "Scala")
+        val setTask11List = mutableListOf<String>()
+        for (elem in setTask11) {
+            setTask11List.add(elem)
+        }
+        println("setTask11 $setTask11 setTask11List $setTask11List")
+    }
+
+    //Вспомогательная функция для проверки наличия элемента в множестве
+    private fun checkSet(set: Set<String>, str: String): Boolean {
+        for (elem in set) {
+            if (elem == str) return true
+        }
+        return false
+    }
 }
 
-fun checkSet(set: Set<String>, str: String): Boolean {
-    for (elem in set) {
-        if (elem == str) return true
-    }
-    return false
+fun main() {
+    val lesson12 = Lesson10_Sets()
+    lesson12.setTask1()
+    lesson12.setTask2()
+    lesson12.setTask3()
+    lesson12.setTask4()
+    lesson12.setTask5()
+    lesson12.setTask6()
+    lesson12.setTask7()
+    lesson12.setTask8()
+    lesson12.setTask9()
+    lesson12.setTask10()
+    lesson12.setTask11()
 }
